e2311eac216774861aeb7a743ea0ec8f53d257f8
raiaan  mustfa <raiaanm@gmail.com>
2019-05-23 02:14:14 +0200

    my second angular project

---
+++ users-app/src/app/data.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http'
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  constructor(private http : HttpClient) { }
+  getUsers(){
+    return this.http.get('https://jsonplaceholder.typicode.com/users');
+  }
+  getUser(UserId){
+    return this.http.get('https://jsonplaceholder.typicode.com/users/'+UserId);
+  }
+  getPosts(){
+    return this.http.get('https://jsonplaceholder.typicode.com/posts');
+  }
+}
